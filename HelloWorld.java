3b9ba3244d840fb013bb199c1c56aa0c97db5a0e
Jothsna <jothsnareddytester@gmail.com>
2025-03-20 11:34:32 +0000

    Committed

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Program created for jenkins assignments");
+
+	}
+
+}
